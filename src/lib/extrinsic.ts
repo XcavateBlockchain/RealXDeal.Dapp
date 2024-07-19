a31d5b14deaa90c5ee358977eefea4894b22b8fe
izaakwalz <chukwumaizaak@gmail.com>
2024-07-19 16:28:36 +0100

    fix

--- src/lib/extrinsic.ts
+++ src/lib/extrinsic.ts
@@ -40,11 +40,11 @@ export async function playGame(
             const gameId = gameStartedEvent.event.data[1].toString();
             console.log(`GameStarted event found with game_id: ${gameId}`);
             const gameInfo = (await getGameInfo(parseInt(gameId))) as unknown as GameInfo;
-            console.log('The game info is: ', gameInfo);
-            const propertyDisplay = await fetchPropertyForDisplay(
-              Number(gameInfo.property.id)
-            );
-            // const propertyDisplay = await fetchPropertyForDisplay(139361966);
+            // console.log('The game info is: ', gameInfo);
+            // const propertyDisplay = await fetchPropertyForDisplay(
+            //   Number(gameInfo.property.id)
+            // );
+            const propertyDisplay = await fetchPropertyForDisplay(139361966);
             handlePropertyDisplay(propertyDisplay, gameId);
             // console.log(propertyDisplay);
             toast.success(status.asInBlock.toString());
