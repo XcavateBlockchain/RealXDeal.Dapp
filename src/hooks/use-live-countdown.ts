2acaafdd072f9fa316743250d45d2377dcbe7e24
Connor Campbell <ccampbell.james@gmail.com>
2024-08-14 17:07:38 +0200

    feat: return number of blocks remaining instead

--- src/hooks/use-live-countdown.ts
+++ src/hooks/use-live-countdown.ts
@@ -14,24 +14,80 @@
 //   return { seconds };
 // }
 
+// import { useEffect, useState } from 'react';
+// import { ApiPromise } from '@polkadot/api';
+// import { getApi } from '@/lib/polkadot';
+
+// export default function useLiveCountdown(
+//   // api: ApiPromise | null,
+//   currentBlockNumber: number,
+//   expiryBlockNumber: number
+// ) {
+//   const initialSeconds = (expiryBlockNumber - currentBlockNumber) * 6;
+//   const [seconds, setSeconds] = useState(initialSeconds);
+//   const [blockCountdown, setBlockCountdown] = useState(6);
+
+//   useEffect(() => {
+//     // if (!api) return;
+
+//     let intervalId: NodeJS.Timeout;
+
+//     const startCountdown = async () => {
+//       const api = await getApi();
+//       const unsubscribe = await api.rpc.chain.subscribeNewHeads(lastHeader => {
+//         const newBlockNumber = lastHeader.number.toNumber();
+//         const remainingBlocks = expiryBlockNumber - newBlockNumber;
+
+//         if (remainingBlocks > 0) {
+//           const newTimeLeft = remainingBlocks * 6;
+//           setSeconds(newTimeLeft);
+//           setBlockCountdown(6); // Reset block countdown to 6 seconds
+//         } else {
+//           clearInterval(intervalId);
+//           setSeconds(0);
+//           unsubscribe();
+//         }
+//       });
+
+//       // Decrement the time left every second
+//       intervalId = setInterval(() => {
+//         setBlockCountdown(prev => (prev > 1 ? prev - 1 : 6));
+
+//         setSeconds(prevSeconds => {
+//           if (prevSeconds > 0) {
+//             return prevSeconds - 1;
+//           } else {
+//             clearInterval(intervalId);
+//             return 0;
+//           }
+//         });
+//       }, 1000);
+
+//       // Cleanup: Unsubscribe and clear the interval when the component unmounts
+//       return () => {
+//         clearInterval(intervalId);
+//         unsubscribe();
+//       };
+//     };
+
+//     startCountdown();
+//   }, [currentBlockNumber, expiryBlockNumber]);
+
+//   return { seconds };
+// }
+
 import { useEffect, useState } from 'react';
-import { ApiPromise } from '@polkadot/api';
 import { getApi } from '@/lib/polkadot';
 
 export default function useLiveCountdown(
-  // api: ApiPromise | null,
   currentBlockNumber: number,
   expiryBlockNumber: number
 ) {
-  const initialSeconds = (expiryBlockNumber - currentBlockNumber) * 6;
-  const [seconds, setSeconds] = useState(initialSeconds);
-  const [blockCountdown, setBlockCountdown] = useState(6);
+  const [blocksRemaining, setBlocksRemaining] = useState(
+    expiryBlockNumber - currentBlockNumber
+  );
 
   useEffect(() => {
-    // if (!api) return;
-
-    let intervalId: NodeJS.Timeout;
-
     const startCountdown = async () => {
       const api = await getApi();
       const unsubscribe = await api.rpc.chain.subscribeNewHeads(lastHeader => {
@@ -39,33 +95,15 @@ export default function useLiveCountdown(
         const remainingBlocks = expiryBlockNumber - newBlockNumber;
 
         if (remainingBlocks > 0) {
-          const newTimeLeft = remainingBlocks * 6;
-          setSeconds(newTimeLeft);
-          setBlockCountdown(6); // Reset block countdown to 6 seconds
+          setBlocksRemaining(remainingBlocks);
         } else {
-          clearInterval(intervalId);
-          setSeconds(0);
+          setBlocksRemaining(0);
           unsubscribe();
         }
       });
 
-      // Decrement the time left every second
-      intervalId = setInterval(() => {
-        setBlockCountdown(prev => (prev > 1 ? prev - 1 : 6));
-
-        setSeconds(prevSeconds => {
-          if (prevSeconds > 0) {
-            return prevSeconds - 1;
-          } else {
-            clearInterval(intervalId);
-            return 0;
-          }
-        });
-      }, 1000);
-
-      // Cleanup: Unsubscribe and clear the interval when the component unmounts
+      // Cleanup: Unsubscribe when the component unmounts
       return () => {
-        clearInterval(intervalId);
         unsubscribe();
       };
     };
@@ -73,5 +111,5 @@ export default function useLiveCountdown(
     startCountdown();
   }, [currentBlockNumber, expiryBlockNumber]);
 
-  return { seconds };
+  return { seconds: blocksRemaining };
 }
